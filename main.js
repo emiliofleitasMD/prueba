201ac757221f103b91884c6fa64ab8c22e280dbc
Emilio Efraín Fleitas Mendoza <emilioefrainfleitasmendoza@MacBook-Pro-de-Emilio.local>
2025-03-16 17:47:52 -0300

    feat: initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+let name1 = "Giovanni"; // string / text
+let age = 23 // number
+let conjuntoDeAmigos = ["Joaquin", "Gio", "Abrahan"] // array de strings
+let conjuntoDeEdad = [23,1,3]
+
+let objecto = {
+    name: "Adan",
+    age: 23,
+    conjunto: ["hello", "hola"]
+}
+
+function aumentarEdad(value) {
+    age += value
+}
+aumentarEdad(20);
+
+
+
+console.log(age)
